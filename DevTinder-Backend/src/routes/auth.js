fab0afa7a6c24735f5e78f98fba34dd88000aa1d
prince-kr-dev <princekumark2004@gmail.com>
2025-09-02 02:38:08 +0530

    fixed

--- DevTinder-Backend/src/routes/auth.js
+++ DevTinder-Backend/src/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const authRouter = express.Router();
-const {User} = require("../models/user");
+const { User } = require("../models/user");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const { validateSignupData } = require("../utils/validation");
@@ -10,25 +10,16 @@ authRouter.post("/signup", async (req, res) => {
   try {
     //Validate the data
     validateSignupData(req);
-    const {
-      firstName,
-      lastName,
-      email,
-      password,
-      age,
-      gender,
-      about,
-      skills,
-    } = req.body;
+    const { firstName, lastName, email, password, age, gender, about, skills } =
+      req.body;
     //Encrypt the password
     const passwordHash = await bcrypt.hash(password, 10);
 
-    const checkEmail=await User.findOne({ email });
-    console.log(checkEmail)
-    if(checkEmail){
-      throw new Error("Email Already Exist")
+    const checkEmail = await User.findOne({ email });
+    console.log(checkEmail);
+    if (checkEmail) {
+      throw new Error("Email Already Exist");
     }
-
 
     const user = new User({
       firstName,
@@ -43,7 +34,10 @@ authRouter.post("/signup", async (req, res) => {
     const savedUser = await user.save();
     const token = await savedUser.getJWT();
     res.cookie("token", token, {
-      expires: new Date(Date.now() + 8 * 3600000),
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      maxAge: 8 * 3600000, // 8 hours
     });
     res
       .status(200)
@@ -59,7 +53,7 @@ authRouter.post("/login", async (req, res) => {
     if (!validator.isEmail(email)) {
       throw new Error("Invalid Email");
     }
-    const user = await User.findOne({email});
+    const user = await User.findOne({ email });
     if (!user) {
       throw new Error("Invalid Credentials");
     }
@@ -67,7 +61,10 @@ authRouter.post("/login", async (req, res) => {
     if (isValidPassword) {
       const token = await user.getJWT();
       res.cookie("token", token, {
-        expires: new Date(Date.now() + 8 * 3600000),
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        maxAge: 8 * 3600000, // 8 hours
       });
       res.status(200).json({ user });
     } else {
