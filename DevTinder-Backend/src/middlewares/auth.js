950d8458804660ee8cebc0bc44c5043b4902bdd6
prince-kr-dev <princekumark2004@gmail.com>
2025-09-02 02:30:05 +0530

    fixed

--- DevTinder-Backend/src/middlewares/auth.js
+++ DevTinder-Backend/src/middlewares/auth.js
@@ -1,33 +1,30 @@
 const jwt = require("jsonwebtoken");
-const {User} = require("../models/user");
+const { User } = require("../models/user");
 
-const userAuth = async(req,res, next)=>{
-    try{
-        //Read the token from the request cookies
-        const cookie = req.cookies;
-    
-        const {token} = cookie;
+const userAuth = async (req, res, next) => {
+  try {
+    // Read the token from the request cookies
+    const token = req.cookies?.token;
 
-        if(!token){
-            return res.status(401).send("Please Login");
-        }
-    
-        const decodedObj = await jwt.verify(token, process.env.JWT_SECRET);
-    
-        const {_id} = decodedObj;
-    
-        const user = await User.findById(_id);
-    
-        if(!user){
-            throw new Error("User not found");
-        }
+    if (!token) {
+      return res.status(401).send("Please Login");
+    }
 
-        req.user = user;
-        next();
+    // Verify token
+    const decodedObj = jwt.verify(token, process.env.JWT_SECRET);
 
-    } catch (err){
-        res.status(400).send("ERROR : " + err.message);
+    // use the correct key name (id, not _id)
+    const user = await User.findById(decodedObj.id);
+
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
   }
+};
 
-}
-module.exports ={userAuth}
+module.exports = { userAuth };
